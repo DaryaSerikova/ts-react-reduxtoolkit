70aa1e24ced68d5ebc83142cf1e4f5000ad384c8
DaryaSerikova <dvserikova@gmail.com>
2022-11-16 00:56:31 +0700

    ErrorPage: white background, h1; ContactPage: scss cleaning

--- src/pages/Error404Page/Error404Page.js
+++ src/pages/Error404Page/Error404Page.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { ReactComponent as HomeRun } from "../../assets/img/home_run_2.svg"
 import { ReactComponent as YellowDots } from '../../assets/img/onenews_yellow_dots.svg';
-
+import background from "../../assets/img/error404_background.png";
 import cn from "./Error404Page.module.scss";
 
 const Error404Page = () => {
   return (
     <main className={`${cn.parent}`}>
-    {/* <PurpleBackground1 /> */}
       <div className={`${cn.child_background} ${cn.purple_background}`}></div>
+      <div className={`${cn.child_background} `}>
+        <img className={cn.white_background} src={background} alt={"white-background"}/>
+      </div>
+      
       {/* <div className={cn.child_background}></div> */}
 
       <div className={`${cn.container} ${cn.child_content}`}>
+      
 
         <div className={cn.page_error404}>
         
@@ -19,7 +23,7 @@ const Error404Page = () => {
             <div className={`${cn.child_background_2}`}>
               <YellowDots />
             </div>
-            <div className={cn.title}>Ошибка 404</div>
+            <h1 className={cn.title}>Ошибка 404</h1>
             <div className={cn.text}>Возможно, у вас опечатка в адресе страницы, или её просто не существует</div>
             <button className={cn.btn}>
               <div className={cn.btn_icon}>
@@ -33,6 +37,7 @@ const Error404Page = () => {
             <div className={cn.huge_error404}>404</div>
             <div className={`${cn.child_background_3}`}>
               <YellowDots />
+              {/* WhiteDots !!!!!!!!!!!!!!!!!!!!!!! */}
             </div>
           </div>
 
